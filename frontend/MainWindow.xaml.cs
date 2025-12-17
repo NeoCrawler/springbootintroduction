eb81b83a48d53a4f82dfb64122f33c9ca15c1214
NeoCrawler <neocrawler@hotmail.com>
2025-12-17 16:37:43 +0100

    Remove unused includes.

--- frontend/MainWindow.xaml.cs
+++ frontend/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using Newtonsoft.Json;
-using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SpringBootInterface;
 
@@ -81,7 +71,7 @@ public partial class MainWindow : Window
 
         var content = await response.Content.ReadAsStringAsync();
 
-        // Display result.
+        // Display result box.
         txtResult.Visibility = Visibility.Visible;
         txtResult.Text = content == "true" ? "Correct!" : "Wrong!";
 
